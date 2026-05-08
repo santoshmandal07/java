462972623d446384b1000462002b39777d917a8c
Santosh Mandal <139973330+sannn9090@users.noreply.github.com>
2026-05-08 12:33:33 +0530

    new java

--- Basic Code/box.java
+++ Basic Code/box.java
@@ -1,24 +1,32 @@
 class Box_Demo{
+    //declare variables
     double width;
     double height;  
     double depth;
+
+    //volume method
+    void volume(){
+        double vol = width * height * depth;
+        System.out.println("Volume is " + vol);
+    }
 }
+
+//volume method
 
 class box{
     public static void main(String args[]){
+        //1st box
         Box_Demo mybox1 = new Box_Demo();
-        double vol;
         mybox1.width = 10;
         mybox1.height = 20;
         mybox1.depth = 30;
-        vol = mybox1.width * mybox1.height * mybox1.depth;
-        System.out.println("Volume is " + vol);
+        mybox1.volume();
 
+        //2nd box
         Box_Demo mybox2 = new Box_Demo();
         mybox2.width = 40;       
         mybox2.height = 50;
         mybox2.depth = 60;
-        vol = mybox2.width * mybox2.height * mybox2.depth;
-        System.out.println("Volume is " + vol);
+        mybox2.volume();
     }
 }
